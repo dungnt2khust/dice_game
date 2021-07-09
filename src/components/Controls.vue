c198bce656bea423629ef5ef62ca7aa809badb71
Dung <dunggamo2000@gmail.com>
2021-07-09 22:44:02 +0700

    Hold score event

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -6,7 +6,9 @@
         <button
             v-on:click="$emit('handleRollDice')"
             class="control btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
-        <button class="control btn-hold"><i class="ion-ios-download-outline"></i>Hold</button>
+        <button 
+            v-on:click="$emit('handleHoldScore')"
+            class="control btn-hold"><i class="ion-ios-download-outline"></i>Hold</button>
         
         <input type="number" placeholder="Final score" class="final-score">
     </div>
